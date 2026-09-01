3c5ccf31624f0c9b17d8abd4859262bd27ae86c6
Agus26600 <a21200707@alumnos.uady.mx>
2026-09-01 13:49:39 -0600

    EntidadPaciente

--- src/main/java/com/signosvitales/model/EntidadPaciente.java
+++ src/main/java/com/signosvitales/model/EntidadPaciente.java
@@ -1,5 +1,34 @@
 package com.signosvitales.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EntidadPaciente {
-    
+    //variables iniciales para info del paciente
+    private String idPaciente;
+    private String nombre;
+    private int edad;
+    private List<RegistroSignoVital> historial;
+
+    public EntidadPaciente(String idPaciente, String nombre, int edad) {
+        this.idPaciente = idPaciente;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.historial = new ArrayList<>();
+    }
+
+    // Método principal, se conecta con RegistroSignoVital
+    public void agregarRegistro(RegistroSignoVital registro) {
+        this.historial.add(registro);
+    }
+
+    // Getters y Setters
+    public String getIdPaciente() { return idPaciente; }
+    public void setIdPaciente(String idPaciente) { this.idPaciente = idPaciente; }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public int getEdad() { return edad; }
+    public void setEdad(int edad) { this.edad = edad; }
+    public List<RegistroSignoVital> getHistorial() { return historial; }
+    public void setHistorial(List<RegistroSignoVital> historial) { this.historial = historial; }
 }
